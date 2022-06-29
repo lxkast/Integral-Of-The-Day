5eb982ad4f3c4892236c4b6f194d0778a766f032
Lukas <86862094+lxkast@users.noreply.github.com>
2022-06-29 19:19:29 +0100

    ⚙️Mostly finished API

--- API.py
+++ API.py
@@ -1,32 +1,64 @@
 from re import L
-import schedule
+from apscheduler.schedulers.background import BackgroundScheduler
 import random
 from flask import Flask, jsonify, request
 import linecache
 
-todaysIntegral = ""
+todaysIntegral = "http://i.ytimg.com/vi/YI1x2bgAwAA/maxresdefault.jpg"
+usedIntegrals = [137]
+
+def schedulerTest():
+    print("scheduler works !!")
 
 def generateIntegral():
     with open(r"ActiveIntegrals.txt", "r") as fp:
-        for count, line in enumerate (fp): #  count how many integrals are left
-            pass
-        print("Integrals left: " + count)
-        if count <= 50: print("RUNNING OUT OF INTEGRALS!")
-        todaysLine = random.randint(1, count)
+        total = 579
+        
+        repeat = False
+        freshIntegral = False
+        while freshIntegral == False:
+            todaysLine = random.randint(1, total)
+        
+            for i in range(0,len(usedIntegrals)):
+                if usedIntegrals[i] == todaysLine:
+                    repeat = True
+                    break
+            if repeat == False: freshIntegral = True
+
+
         todaysIntegral = linecache.getline(r"ActiveIntegrals.txt", todaysLine)
         lines = []
         lines = fp.readlines()
+        print(usedIntegrals)
+        
 
-    with open(r"ActiveIntegrals.txt", 'w') as fp:
-        for number, line in enumerate(lines):
-            if number not in [todaysLine]:
-                fp.write(line)
+    #with open("ActiveIntegrals.txt", "w") as f:
+    #    for line in lines:
+    #        if line.strip("\n") != todaysIntegral:
+    #            f.write(line)
+    #
+    #
+    # WHY CANT I DELETE A SPECIFIC LINE FROM A TEXT FILE IT JUST DELETES EVERYTHING
+    #
+    # NOW IM GOING TO MAKE IT SUPER INEFFICIENT
+    #
+    # I AM GOIN G INSANE
+    #
     
- 
+    usedIntegrals.append(todaysLine)
+    
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(generateIntegral,'interval',seconds=2)
+sched.start()
+
 app = Flask(__name__)
 
 @app.route("/integral", methods=['GET'])   
 
 def integral():
     data = request.get_json()
-    schedule.every().day.at("00:00").do(generateIntegral)
+    return jsonify({"integral": todaysIntegral})
+
+if __name__ == "__main__":
+    app.run()
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="http://i.ytimg.com/vi/PrvJrUvj5Pk/maxresdefault.jpg"/>
+    </div>
+  );
+}
+
+export default App;
